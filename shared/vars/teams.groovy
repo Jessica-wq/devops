7d3a5596910ec13018ba9772a12e3f4eb6989386
Jessica-wq <71296968+Jessica-wq@users.noreply.github.com>
2024-03-11 17:51:37 -0500

    Create teams.groovy

---
+++ shared/vars/teams.groovy
@@ -0,0 +1,12 @@
+def notificationMail(){
+            def teamsChannelUrl = 'https://teams.microsoft.com/l/channel/19%3A73438f87424641d0ae879cfdcbb9e5e8%40thread.tacv2/DEVOPS_JENKINS?groupId=9001802f-67b8-4dc5-a438-1d39237ea821&tenantId=10a76712-94f6-46a2-9155-31bd8b76f937'
+            def teamsWebhookUrl = 'https://graval.webhook.office.com/webhookb2/9001802f-67b8-4dc5-a438-1d39237ea821@10a76712-94f6-46a2-9155-31bd8b76f937/JenkinsCI/9507d970aa864a2e898155f967de45d4/b09a9b54-b1f8-4213-82b9-07d0a8a5d417'
+            def jobName = "prueba"
+            def buildStatus = "SUCCESS"
+            def message = """
+            **hola mundo**
+  
+
+            """
+
+}
