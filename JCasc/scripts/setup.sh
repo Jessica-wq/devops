0a46e117bfff89c9cf5bab3ed6d6365f22f294fd
Jessica-wq <71296968+Jessica-wq@users.noreply.github.com>
2024-03-11 18:13:48 -0500

    Create setup.sh

---
+++ JCasc/scripts/setup.sh
@@ -0,0 +1,6 @@
+﻿#!/bin/bash
+mv /tmp/plugins.txt /usr/share/jenkins/ref/plugins.txt
+bash jenkins-plugin-cli -f /usr/share/jenkins/ref/plugins.txt
+mv /tmp/init.groovy /usr/share/jenkins/ref/init.groovy.d/init.groovy
+mkdir -p /usr/share/jenkins/ref/config
+mv /tmp/*.yaml /usr/share/jenkins/ref/config/
